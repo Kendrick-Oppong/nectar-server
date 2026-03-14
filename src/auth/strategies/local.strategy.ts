962e81d5d9e6159824721e0f5751767b68ecc327
Kendrick <kendrickoppong94@gmail.com>
2026-03-14 19:37:31 +0000

    feat: remove JwtStrategy and add JwtAuthGuard; update LocalStrategy to use UserType

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,19 +1,16 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { User } from '../../../generated/prisma/client';
 import { AuthService } from '../auth.service';
+import type { UserType } from 'types/auth';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
+  constructor(private readonly authService: AuthService) {
     super({ usernameField: 'email' });
   }
 
-  async validate(
-    email: string,
-    password: string,
-  ): Promise<Omit<User, 'password' | 'failedLoginAttempts' | 'lockedUntil'>> {
+  async validate(email: string, password: string): Promise<UserType> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
       throw new UnauthorizedException('Invalid credentials');

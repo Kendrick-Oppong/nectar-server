34b8fb03fb7ec45fbaaabca42aca16e4bbe1b352
Kendrick <kendrickoppong94@gmail.com>
2026-03-28 09:56:17 +0000

    set Global Prefix

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
     }),
   );
 
+  app.setGlobalPrefix('api');
+
   // Swagger Configuration
   const config = new DocumentBuilder()
     .setTitle('Nectar Grocery App API')
@@ -27,7 +29,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
   const port = process.env.PORT ?? 3000;
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
   console.log(`Swagger docs: http://localhost:${port}/api/docs`);
 }

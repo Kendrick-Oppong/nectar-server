2684e856c98c1550599feb082fb39b9bc58d2558
Kendrick-Oppong <kendrickoppong94@gmail.com>
2026-04-30 22:32:01 +0000

    Remove exclamation mark from greeting message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Nectar API is running!';
+    return 'Nectar API is running';
   }
 }
